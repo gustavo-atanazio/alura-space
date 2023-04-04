cc646da377603f92ee38b8a034e6f1a93c25f210
gustavo-atanazio <gus.atanazio@gmail.com>
2023-04-04 11:21:38 -0300

    Aplicando o menu e adicionando o banner

--- src/paginas/PaginaInicial/index.jsx
+++ src/paginas/PaginaInicial/index.jsx
@@ -1,8 +1,22 @@
 import Cabecalho from "../../componentes/Cabecalho";
+import Menu from "../../componentes/Menu";
+import banner from './banner.png'
+import styles from './PaginaInicial.module.scss'
 
 function PaginaInicial() {
     return (
+        <>
         <Cabecalho/>
+        <main>
+            <section className={styles.principal}>
+                <Menu/>
+                <div className={styles.principal__imagem}>
+                    <h1>A galeria mais completa do espaço</h1>
+                    <img src={banner} alt="Terra vista do espaço" />
+                </div>
+            </section>
+        </main>
+        </>
     )
 }
 
