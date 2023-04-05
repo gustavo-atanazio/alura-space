273548c1deb051d0edadbe17655e3419b378aca9
gustavo-atanazio <gus.atanazio@gmail.com>
2023-04-05 11:45:02 -0300

    Importando o normalize.css

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+import "./assets/fontes/GandhiSans-Regular.otf";
+import "./assets/fontes/GandhiSans-Bold.otf";
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
