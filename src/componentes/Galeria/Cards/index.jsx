8bd773bf0e5515e62bf833bccaaa3189842245ad
gustavo-atanazio <gus.atanazio@gmail.com>
2023-04-06 11:03:32 -0300

    Refatoração dos itens da galeria

--- src/componentes/Galeria/Cards/index.jsx
+++ src/componentes/Galeria/Cards/index.jsx
@@ -1,26 +1,10 @@
 import React from 'react'
-import open from './open.png'
-import favorito from './favorito.png'
+import Card from './Card'
 
 export default function Cards({fotos, styles}) {
     return (
         <ul className={styles.galeria__cards}>
-            {fotos.map(foto => {
-                return (
-                    <li key={foto.id} className={styles.galeria__card}>
-                        <img src={foto.imagem} alt={foto.titulo} className={styles.galeria__imagem} />
-                        <p className={styles.galeria__descricao}>{foto.titulo}</p>
-
-                        <div>
-                            <p>{foto.creditos}</p>
-                            <span>
-                                <img src={favorito} alt="ícone de coração para curtir" />
-                                <img src={open} alt="ícone de expandir imagem" />
-                            </span>
-                        </div>
-                    </li>
-                )
-            })}
+            {fotos.map(foto => <Card key={foto.id} foto={foto} styles={styles}/>)}
         </ul>
     )
 }
